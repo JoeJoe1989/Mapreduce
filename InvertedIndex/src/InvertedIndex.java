e51e7b3eb019895a6baa1976de385ff8bca9e670
JoeJoe1989 <josephzhou2011@gmail.com>
2015-04-24 22:28:41 -0400

    map

--- InvertedIndex/src/InvertedIndex.java
+++ InvertedIndex/src/InvertedIndex.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
@@ -15,40 +17,96 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class InvertedIndex {
+
 	public static class Map extends
 			Mapper<NullWritable, BytesWritable, Text, Text> {
 
 		public void map(NullWritable key, BytesWritable value, Context context)
 				throws IOException, InterruptedException {
 
+			HashMap<String, ArrayList<Occurence>> wordOccurence = new HashMap<String, ArrayList<Occurence>>();
+
 			Text keyInfo = new Text();
 			Text valueInfo = new Text();
-			
+
 			Path filePath = ((FileSplit) context.getInputSplit()).getPath();
-			String fileNameString = filePath.getName();
+			String url = filePath.getName();
 
 			byte[] fileContentByte = value.getBytes();
-			
-			
+
 			String html = new String(fileContentByte);
 			Document doc = Jsoup.parse(html);
 
-			String body = doc.body().text();
-			StringTokenizer tokens = new StringTokenizer(body);
-			
 			int position = 0;
-			
-			while(tokens.hasMoreTokens()){
-				keyInfo.set(tokens.nextToken());
-				valueInfo.set(position + "::" + fileNameString);
-				context.write(keyInfo, valueInfo);
+
+			// for meta data
+			Elements metaData = doc.select("meta[name=description]");
+
+			String meta = metaData.select("content").first().toString();
+			StringTokenizer metaTokens = new StringTokenizer(meta);
+			while (metaTokens.hasMoreTokens()) {
+				String word = metaTokens.nextToken();
+				helper(url, wordOccurence, word, 2, 3, position);
 				position++;
 			}
 
-		}
+			// for title
+			String title = doc.title();
+			StringTokenizer titleTokens = new StringTokenizer(title);
+			while (titleTokens.hasMoreTokens()) {
+				String word = metaTokens.nextToken();
+				helper(url, wordOccurence, word, 1, 5, position);
+				position++;
+			}
 
+			// for body
+			String body = doc.body().text();
+			StringTokenizer bodyTokens = new StringTokenizer(body);
+
+			while (bodyTokens.hasMoreTokens()) {
+				String word = bodyTokens.nextToken();
+				helper(url, wordOccurence, word, 3, 1, position);
+				position++;
+			}
+
+			// for anchor
+			Elements links = doc.select("a[href]");
+			int numOutLinks = links.size();
+
+			for (Element link : links) {
+				String anchor = link.text().trim();
+				StringTokenizer anchorTokens = new StringTokenizer(body);
+				String outLink = link.attr("abs:href").toString();
+
+				keyInfo.set("Link\t" + url);
+				valueInfo.set(numOutLinks + "," + outLink);
+				context.write(keyInfo, valueInfo);
+
+				while (anchorTokens.hasMoreTokens()) {
+					String word = anchorTokens.nextToken();
+					helper(outLink, wordOccurence, word, 0, 10, 0);
+				}
+			}
+
+			for (String word : wordOccurence.keySet()) {
+				keyInfo.set("Word\t" + word);
+				String output = "";
+				int size = wordOccurence.get(word).size();
+				for (int i = 0; i < size; i++) {
+					output += wordOccurence.get(word).toString();
+					if (i != size - 1)
+						output += "||";
+				}
+
+				valueInfo.set(output);
+				context.write(keyInfo, valueInfo);
+			}
+
+		}
 	}
 
 	public static class Reduce extends Reducer<Text, Text, Text, Text> {
@@ -58,14 +116,32 @@ public class InvertedIndex {
 		public void reduce(Text key, Iterable<Text> values, Context context)
 				throws IOException, InterruptedException {
 
-			// System.out.println("##################################################");
-			String result = "";
-			for (Text val : values) {
-				result += val.toString() + "\t";
-			}
-			valueInfo.set(result);
-			context.write(key, valueInfo);
 		}
+	}
+
+	private static void helper(String url,
+			HashMap<String, ArrayList<Occurence>> wordOccurence, String word,
+			int type, int importance, int position) {
+
+		int isCapital = 0;
+
+		if (isAllLetter(word)) {
+			if (isAllCapital(word)) {
+				isCapital = 1;
+			}
+			word = stem(word.toLowerCase().toString());
+		}
+
+		if (wordOccurence.containsKey(word)) {
+			ArrayList<Occurence> tempList = new ArrayList<Occurence>();
+			tempList.add(new Occurence(url, isCapital, type, importance,
+					position));
+			wordOccurence.put(word, tempList);
+		} else {
+			wordOccurence.get(word).add(
+					new Occurence(url, isCapital, type, importance, position));
+		}
+
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -77,15 +153,12 @@ public class InvertedIndex {
 
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(Text.class);
-		
+
 		job.setInputFormatClass(WholeFileInputFormat.class);
 		job.setOutputFormatClass(TextOutputFormat.class);
 
-		
 		job.setMapperClass(Map.class);
 		job.setReducerClass(Reduce.class);
-
-//		job.setNumReduceTasks(0);
 
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
